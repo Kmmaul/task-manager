3c0ca6962275368a8e113c3d8bf495526f3e04e8
Kmmaul <kelmaul00@hotmail.com>
2026-04-27 17:17:32 -0700

    Day 1: working task manager script

---
+++ index.js
@@ -0,0 +1,21 @@
+const tasks = [];
+
+function addTask(task)
+{
+    tasks.push(task);
+    console.log("Task added:", task);
+}
+
+function listTasks()
+{
+    console.log("Your tasks:");
+    tasks.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+    });
+}
+
+
+//test
+addTask("learn JavaScript");
+addTask("Start my dev career");
+listTasks();
