85ce9eaa162cf9e8b012a12c131827010b3579e4
Kmmaul <kelmaul00@hotmail.com>
2026-04-29 17:00:29 -0700

    Connect frontend and enable task creation via API

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 let tasks = [
     {id: 1, title: "Learn JavaScript", completed: false},
     {id: 2, title: "Build an API", completed: false}
