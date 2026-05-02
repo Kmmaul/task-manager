d32422746f60c9a82e2c5d86b5a3a3cfe7982ec1
Kmmaul <kelmaul00@hotmail.com>
2026-05-01 17:08:20 -0700

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(require("cors")());
 
 
 // let tasks = [
